f492ef7dc03e9bb4e8ee06e4870255186b51ff95
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-01-19 14:08:39 +0530

    File is basically about how to link js file with html file

---
+++ jschapter2.js
@@ -0,0 +1,51 @@
+console.log("hello");
+console.log("This is our JS program");
+let a=23;
+let b=56;
+sum=a+b;
+console.log(sum);
+
+let x=87;
+let y=89;
+let totalprice;
+//console.log("total price is : " ,x+y,"ruppes .");
+//let output=`the total price is: ${totalprice} Ruppes.`; //backtick
+console.log(`the total price is: ${x+y} Ruppes.`);
+
+
+let p=89;
+let q=87;
+console.log(p+q);
+console.log(p/q);
+console.log(p%q);
+console.log(p<q);
+console.log(p>=q);
+console.log(p<+q);
+console.log(p++);
+console.log(++q);
+console.log(p==q);
+console.log(p===q);
+console.log(p-q);
+
+let age=89;
+if(age>=18){
+    console.log("CONGRATULATIONS , YOU CAN VOTE");
+}else{
+    console.log("SORRY, YOU CAN'T VOTE");
+}
+
+let m1=90;
+if(m1>=35){
+    console.log("pass");
+    if(m1>=80){
+        console.log("GRADE A");
+    }else{
+        console.log('GRADE 0');
+    }
+}else{
+    close.log("fail");
+}
+ 
+let priya=45;
+let kriya=98;
+console.log(priya&&kriya);
