bf4eff52623dc9f1d6bf2b10e1381a6ce8e7ae78
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-01-20 14:17:50 +0530

    Chapter 3 of js and all string methods in javascript

---
+++ jschapter3.js
@@ -0,0 +1,25 @@
+let name = "   h ello oo   ";
+
+console.log(name);          // original
+
+name = name.trim();
+console.log(name);          // trim ke baad
+
+name = name.toLowerCase();
+console.log(name);          // lowercase
+
+name = name.toUpperCase();
+console.log(name);          // uppercase
+
+let msg="hieee";
+let repeat=msg.repeat(4);
+console.log(repeat);
+let str="i love javascript";
+let index=str.indexOf("love");
+console.log(index);
+let replace=str.replace("love","do");
+console.log(replace);
+let slice=str.slice(2 ,5);
+console.log(slice);
+strr="apnacollege";
+let slicee=strr.slice(-3); //11-3;
