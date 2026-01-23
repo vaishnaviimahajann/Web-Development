032d11b2779ef870f643ca92cfe84896cef50cb3
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-01-23 20:36:22 +0530

    library system .. done half of the work of library sytem

--- jschapter3.js
+++ jschapter3.js
@@ -23,3 +23,21 @@ let slice=str.slice(2 ,5);
 console.log(slice);
 strr="apnacollege";
 let slicee=strr.slice(-3); //11-3;
+
+let student=["raj","vaishnavi"];
+console.log(student[0]);
+console.log(student[1]);
+console.log(student[2]);
+
+
+let mixarray=["om","hello",56,78.65];
+console.log(mixarray[0]);
+console.log(mixarray[1]);
+console.log(mixarray[2]);
+console.log(mixarray[3]);
+
+
+let nums=[56,45,76,89,43,33];
+console.log(nums[0]);
+console.log(nums[1]);
+console.log(nums[2]);
