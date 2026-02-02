b208efe00bf1b1243621fa46e148e477a4270e3d
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-02 18:32:28 +0530

    learn new thing like what is dom how we cam access elemnts or how we can manipulate elemnts se minor changes on console

---
+++ jschapter9.js
@@ -0,0 +1,15 @@
+console.dir(document.querySelector("#mainImg"));
+document.querySelector("#mainImg").src="img2.jpg";
+
+let para=document.querySelector("p");
+console.dir(para);
+para.textContent="Spider";
+para.style.color="blue";
+
+for (links of link){
+    links.style.color=blue;
+}
+
+letpara1=document.querySelector("p");
+para1.innertext="heyy my name vaishnavi";
+document.querySelector('body').append(para1);
