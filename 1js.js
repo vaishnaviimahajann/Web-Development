bd171e8321e62d8081028396236b20c6d0055448
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-14 21:12:16 +0530

    i have created array of color and various diffent text and then applied event listener on it see changes how we can change color on paragh and change text continusoly

--- 1js.js
+++ 1js.js
@@ -1,4 +1,4 @@
-function changetext(){
+/*function changetext(){
     document.getElementById("parabtn").addEventListener("click",function(){
          document.getElementById("para").style.color="red";
 
@@ -11,3 +11,36 @@ function changecolor(){
     })
 }
 changecolor();
+
+let color=["red","green","purple","black","red"];
+let index=0;
+function changecolor(){
+    document.getElementById("btn").addEventListener("click",function(){
+        document.getElementById("mypara").style.color=color[index];
+         index++;
+
+    if(index>=color.length){
+        index=0;
+    }
+    })
+   
+}
+changecolor();*/
+
+let text=[
+    "hello dear",
+    "welcome to javascript language",
+    "you are contineously changing paragrah text",
+    "ok thankyou now u can leave from here",
+];
+let index=0;
+function changetext(){
+    document.getElementById("btn").addEventListener("click",function(){
+        document.getElementById("mypara").innerHTML=text[index];
+        index++;
+        if(index>=text.length){
+        index=0;
+    }
+    })
+}
+changetext();
