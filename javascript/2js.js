841234c15b51ea4b587f255eb4167af3c550217c
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-19 16:27:07 +0530

    created simple counter app in which ihave used some dom properties

---
+++ javascript/2js.js
@@ -0,0 +1,15 @@
+document.getElementById("btn").addEventListener("click",function(){
+          let div=document.querySelectorAll(".div1");
+          for (let i = 0; i < div.length; i++) {
+                     div[i].style.backgroundColor = "yellow";
+}
+})
+let count=0;
+document.getElementById("increase").addEventListener("click",function(){
+    count++;
+    document.getElementById("count").innerText=count;
+})
+document.getElementById("decrease").addEventListener("click",function(){
+    count--;
+    document.getElementById("count").innerText=count;
+})
