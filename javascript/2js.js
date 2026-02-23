63cf313db326d3679186512798ef872894f10285
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-02-23 19:55:19 +0530

    added css to counter app

--- javascript/2js.js
+++ javascript/2js.js
@@ -1,9 +1,9 @@
-document.getElementById("btn").addEventListener("click",function(){
+/*document.getElementById("btn").addEventListener("click",function(){
           let div=document.querySelectorAll(".div1");
           for (let i = 0; i < div.length; i++) {
                      div[i].style.backgroundColor = "yellow";
 }
-})
+})*/
 let count=0;
 document.getElementById("increase").addEventListener("click",function(){
     count++;
