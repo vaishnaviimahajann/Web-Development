62b32f09ca91d90183fac4aa74c6e27c6b55f7ce
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-07 10:00:35 +0530

    learn about react.. setup all the react file

---
+++ REACT/basic-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+//import Title from "./titlle,jsx";
+import Product from "./product.jsx";
+function Title(){
+  return <h1> I am the title</h1>;
+}
+function App() {
+  return(
+    <>
+     <Product></Product>
+      <Product></Product>
+    </>
+  );
+}
+
+export default App;
---
+++ REACT/basic-react-app/src/titlle.jsx
@@ -0,0 +1,6 @@
+function Title(){
+  return <h1> I am the title</h1>;
+}
+
+//export
+export default Title;
