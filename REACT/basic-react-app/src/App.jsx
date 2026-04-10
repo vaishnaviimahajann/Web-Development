d619ad427f6b9adf5edae277732df728da8440fd
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-10 20:23:57 +0530

    uses props from react

--- REACT/basic-react-app/src/App.jsx
+++ REACT/basic-react-app/src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 //import Title from "./titlle,jsx";
 import Product from "./product.jsx";
+import MessageBox from "./messagebox.jsx";
 function Title(){
   return <h1> I am the title</h1>;
 }
 function App() {
-  return(
+  return( 
     <>
-     <Product></Product>
-      <Product></Product>
+     <Product title="Product 1" price={19.99} features={["Feature 1", "Feature 2"]} />
+      <Product title="Product 2" price={29.99} features={["Feature 3", "Feature 4"]} />
+      <MessageBox username="Vaishnavi" message="Welcome to React!" />
     </>
   );
 }
