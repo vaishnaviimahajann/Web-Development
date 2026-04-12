f32a2490838720916833f30502df7b1967b2cf77
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-12 17:05:42 +0530

    .

--- back-end/index.js
+++ back-end/index.js
@@ -1,35 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+// middleware (IMPORTANT)
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080,()=>{
-    console.log('Server is running on port 8080');
-})
-
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+// server start
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
 
-app.get("/user",(req,res)=>{
-    res.send("vaishnavi");
+// GET routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.post("/user", (req, res) => {
-  console.log("BODY:", req.body);
-  res.json(req.body);
+app.get("/user", (req, res) => {
+  res.send("vaishnavi");
 });
-app.get("/userr",(req,res)=>{
-    res.json(
-        {
-            name:"vaishnavi",
-            age:22,
-            city:"pune"
-        }
-    )
+
+app.get("/userr", (req, res) => {
+  res.json({
+    name: "vaishnavi",
+    age: 22,
+    city: "pune",
+  });
 });
 
 app.get("/test", (req, res) => {
   console.log("TEST WORKING");
   res.send("ok");
+});
+
+// POST route
+app.post("/user", (req, res) => {
+  console.log("HEADERS:", req.headers["content-type"]);
+  console.log("BODY:", req.body);
+
+  res.json({
+    message: "Data received successfully",
+    data: req.body,
+  });
 });
