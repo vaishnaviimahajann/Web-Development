8c9d0e39532572593d9a97cde69918569b3af5df
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-05-02 19:11:28 +0530

    A file containing validation rules for the user registration endpoint.

---
+++ back-end/mvc_structure/src/validator/authValidator.js
@@ -0,0 +1,13 @@
+const { body } = require("express-validator");
+
+exports.registerValidator = [
+    body("name").notEmpty().withMessage("Name is required"),
+
+    body("email")
+        .isEmail()
+        .withMessage("Valid email is required"),
+
+    body("password")
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 characters")
+];
